55c0f25d2b3a23b71292b5c54dcb7921dec0564f
Ash <150209573+yoomimoomi@users.noreply.github.com>
2024-10-20 11:33:21 -0400

    css + css vars

---
+++ rewards.js
@@ -0,0 +1,11 @@
+// class dataTable {
+//     table = new Array
+
+//     setItem = {key, value} => {
+
+//     }
+// }
+
+// function rewardEvaluation () {
+//     for (const userId in users)
+// }
